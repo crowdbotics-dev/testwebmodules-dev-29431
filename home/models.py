84503382fe29148237bf9004411f38feb93b3178
Crowdbotics <engineering@crowdbotics.com>
2022-08-18 05:15:53 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class Test123m(models.Model):
     "Generated Model"
     testfield = models.BigIntegerField()
+    ddswx = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
